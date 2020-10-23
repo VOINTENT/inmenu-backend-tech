33eee6b87c8848c1b4855071fa28c108a7743f08
Kirill <k.pechurin02@mail.ru>
2020-10-23 21:28:19 +0300

    add def for static data

--- services/core/docs/db/pgsql/INSERT_ACCOUNT_MAIN.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO account_main(email, hash_password) VALUES
-('k.pech@mail.ru', '12345'),
-('sash@mail.ru', '12345');
--- services/core/docs/db/pgsql/INSERT_PLACE_MAIN.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO place_main( account_main_id,
-                        main_language,
-                        name,
-                        login,
-                        main_currency,
-                        is_draft,
-                        is_published) VALUES
-(1, 1, 'FirstName', 'Login_One', 1, True, True),
-(2, 2, 'SecondName', 'Login_Two', 2, False, False);
