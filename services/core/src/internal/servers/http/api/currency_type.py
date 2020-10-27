923566eb3d95eb384faffd9ce9c1711d00d50809
Kirill-ctrl <54329381+Kirill-ctrl@users.noreply.github.com>
2020-10-27 23:35:09 +0300

    Update currency_type.py

--- services/core/src/internal/servers/http/api/currency_type.py
+++ services/core/src/internal/servers/http/api/currency_type.py
@@ -12,7 +12,7 @@ currency_types = Blueprint('currency_types', url_prefix='/currency_types')
 @currency_types.route('', methods=['GET'])
 @get_pagination_params
 @get_lang_id
-async def get_work_hours(request: Request, pagination_size: int, pagination_after: int, lang_id: int):
+async def get_currency_types(request: Request, pagination_size: int, pagination_after: int, lang_id: int):
     currency_types, err = await CurrencyTypeService.get_currency_types(pagination_size, pagination_after, lang_id)
     if err:
         return err.get_response_with_error()
